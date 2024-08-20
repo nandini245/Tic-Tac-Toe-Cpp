ce45f2ec7d5d4c5a0293b8ee95abd41337ad02fc
nandini245 <142869274+nandini245@users.noreply.github.com>
2024-08-20 22:09:03 +0530

    Update TicTacToeFunctions.cpp

--- TicTacToeFunctions.cpp
+++ TicTacToeFunctions.cpp
@@ -1,127 +1,81 @@
-/*Goal: Practice creating classes and functions. 
-**Create a program that allows two users to 
-**play a 4x4 tic-tac-toe game. 
-*/
-
-#include <fstream>
-
-void getUserNames(string &, string &);
-void printBlankBoard(string);
-void printTheBoard(Board, string);
-void printUserPrompt(string, char);
-void printGameWinner(Board, string, string);
-int  getUserResponse();
-void checkResponse(Board&, char);
-void writeTheBoard(Board);
-
+// TicTacToeFunctions.cpp
+#include <iostream>
+#include <limits>
 using namespace std;
 
-void checkResponse(Board &boardIn, char input)
-{//check that the position selected is not already
-    //selected
-    int position;
-    int userInput;
-    do
-    {
-        position = getUserResponse();
-        userInput = boardIn.setPosition(position, input);
-        if(userInput == -1)
-        {
-            cout<<"That postition is taken.";
-        }
-    }while(userInput == -1);
+void getUserNames(string &userX, string &userO) {
+    cout << "Name of user to be 'x': ";
+    cin >> userX;
+    cout << "Name of user to be 'o': ";
+    cin >> userO;
 }
 
-void getUserNames(string &userX, string &userY)
-{//get the user names
-    std::cout<< "Name of user to be 'x' :";
-    std::cin >>userX;
-    std::cout<< "Name of user to be 'o' :";
-    std::cin >>userY;
-}
-
-void printBlankBoard()
-{//print a blank board, with numbered squares
-    for(int i = 0; i <16; i++)
-    {
-        std::cout<<"|"<<i<<":";
-        if(i <= 9)
-            cout<<" ";
-        if(i%4 == 3)
-        {
-            std::cout<<"|\n";
-        }
+void printBlankBoard() {
+    for (int i = 0; i < 16; i++) {
+        cout << "|" << i << ":";
+        if (i <= 9)
+            cout << " ";
+        if (i % 4 == 3)
+            cout << "|\n";
     }
-    cout<<"\n\n\n";
+    cout << "\n\n\n";
 }
 
-void printTheBoard(Board boardIn)
-{//print the board with player moves
-    printBlankBoard();
-
-    for(int i = 0; i <16; i++)
-    {
-        std::cout<<"|"<<boardIn.getPositions()[i];
-        if(i%4 == 3)
-        {
-            std::cout<<"|\n";
-        }
+void printTheBoard(Board boardIn) {
+    for (int i = 0; i < 16; i++) {
+        cout << "|" << boardIn.getPositions()[i];
+        if (i % 4 == 3)
+            cout << "|\n";
     }
-    cout<<"\n\n\n";
+    cout << "\n\n\n";
 }
 
-void writeTheBoard(Board boardIn)
-{//print the board with player moves
-    cout<<"\n\n";
-    for(int i = 0; i <16; i++)
-    {
-        cout<<"|"<<boardIn.getPositions()[i];
-        if(i%4 == 3)
-        {
-            cout<<"|\n";
-        }
+void writeTheBoard(Board boardIn) {
+    for (int i = 0; i < 16; i++) {
+        cout << "|" << boardIn.getPositions()[i];
+        if (i % 4 == 3)
+            cout << "|\n";
     }
-    cout<<"\n\n\n";
+    cout << "\n\n\n";
 }
 
-void printUserPrompt(string nameIn, char letter)
-{//prompt for user input
-    std::cout<<nameIn<<" where would you like to place an "<<letter<<"?: ";
-    cout<<"\n\n";
-    cout<<" where would you like to place an "<<letter<<"?: ";
+void printUserPrompt(string nameIn, char letter) {
+    cout << nameIn << ", where would you like to place an " << letter << "?: ";
 }
 
-void printGameWinner(Board boardIn, string nameX, string nameO)
-{//print the winner statement
-    char winner;
-    winner = boardIn.determineWinner();
-    if(winner == 'x')
-    {
-        cout<<"Congrats "<<nameX<<" you won!\n\n";
-    }
-    if(winner == 'o')
-    {
-        cout<<"Congrats "<<nameO<<" you won!\n\n";
+void printGameWinner(Board boardIn, string nameX, string nameO) {
+    char winner = boardIn.determineWinner();
+    if (winner == 'x') {
+        cout << "Congrats " << nameX << ", you won!\n\n";
+    } else if (winner == 'o') {
+        cout << "Congrats " << nameO << ", you won!\n\n";
+    } else {
+        cout << "It's a tie!\n\n";
     }
 }
 
-int getUserResponse()
-{//get the chosen user position, check that it is an integer
-    //check that it is between 0 and 15 inclusive
+int getUserResponse() {
     int position = -1;
-    cout<<"Enter an integer between 0 and 15: ";
-    std::cin>>position;
+    cout << "Enter an integer between 0 and 15: ";
+    cin >> position;
     
-    while(position > 15 or position < 0 or !cin)
-    {
+    while (position > 15 || position < 0 || !cin) {
         cin.clear();
-        //cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        std::cout<<"That is not a valid position\n";
-        cout<<"Enter an integer between 0 and 15: ";
-        cin.clear();
-        cin>>position;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "That is not a valid position. Enter an integer between 0 and 15: ";
+        cin >> position;
     }
     return position;
 }
 
-
+void checkResponse(Board &boardIn, char input) {
+    int position;
+    int userInput;
+    do {
+        position = getUserResponse();
+        userInput = boardIn.setPosition(position, input);
+        if (userInput == -1) {
+            cout << "That position is taken.\n";
+        }
+    } while (userInput == -1);
+}
